e5ca46aa9dfc381189e129dc7d3b0651d062a133
Vladislav Zhylenko <zhylenko.vlad@gmail.com>
2021-08-17 14:57:59 +0300

    App lang

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
 @include('layouts.gtm-body', ['id' => 'GTM-59NMKBZ'])
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
